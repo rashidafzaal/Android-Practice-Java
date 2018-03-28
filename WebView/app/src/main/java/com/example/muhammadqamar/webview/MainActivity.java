840683961eaba360b853383f7da2953a3a1a9e1b
Qamar\Muhammad Qamar <handsomerax@gmail.com>
2018-03-28 17:08:19 +0500

    WebView in JAVA

---
+++ WebView/app/src/main/java/com/example/muhammadqamar/webview/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.muhammadqamar.webview;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Make sure your Internet is working
+
+        WebView htmlWebView = (WebView)findViewById(R.id.myWebView);
+        htmlWebView.setWebViewClient(new CustomWebViewClient());
+        WebSettings webSetting = htmlWebView.getSettings();
+        webSetting.setJavaScriptEnabled(true);
+        webSetting.setDisplayZoomControls(true);
+        htmlWebView.loadUrl("http://www.youtube.com/");
+    }
+
+    private class CustomWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
